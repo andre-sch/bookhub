351766ca7802ba7c04fd8368bd1d9f8926051fd6
André Schlichting <andresch.dev@gmail.com>
2025-11-18 08:12:09 -0300

    Log progress during populate script

--- server/src/scripts/populate_db.ts
+++ server/src/scripts/populate_db.ts
@@ -2,6 +2,15 @@ import { loadDataset } from "../infra/openLibrary/loadDataset";
 import { client } from "../infra/pg/connection";
 
 (async function populate() {
+  const start = Date.now();
+  const progress = setInterval(() => {
+    const end = Date.now();
+    const duration = Math.floor((end - start) / 1000);
+    process.stdout.write(`\r(${duration}s) populating database...`);
+  }, 1000);
+
   await loadDataset();
+
+  clearInterval(progress);
   await client.end();
 })();
