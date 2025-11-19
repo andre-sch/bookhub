766a832e0c4b7d6c6737413effb793c015ca6334
André Schlichting <andresch.dev@gmail.com>
2025-11-19 17:31:00 -0300

    Fix category repository adding category level

--- server/src/repositories/CategoryRepository.ts
+++ server/src/repositories/CategoryRepository.ts
@@ -1,9 +1,7 @@
 import { DeweyCategory } from "@/domain/DeweyCategory";
 
-export type DeweyCategoryTree = (DeweyCategory & { level: number })[];
-
 export interface CategoryRepository {
   save(category: DeweyCategory): Promise<void>;
   find(id: string): Promise<DeweyCategory | null>;
-  findHierarchy(id: string): Promise<DeweyCategoryTree>;
+  findHierarchy(decimal: string): Promise<DeweyCategory[]>;
 }
