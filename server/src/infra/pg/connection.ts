63b5f5c5a5dcf1689097baea0883149f1881250a
Diogo Felipe <diogofelipe.dev@gmail.com>
2026-01-29 22:01:49 -0300

    Add: vercel production config updates

--- server/src/infra/pg/connection.ts
+++ server/src/infra/pg/connection.ts
@@ -1,16 +1,15 @@
-import "dotenv/config";
-import { Client } from "pg";
+import 'dotenv/config';
+import { Client } from 'pg';
 
 const client = new Client({
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  database: process.env.DATABASE_NAME
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 client.connect()
-  .then(() => console.log("Connected to database.\n"))
-  .catch(error => console.log("Connection failed.\n", error));
+  .then(() => console.log('Connected to database.\n'))
+  .catch(error => console.error('Connection failed.\n', error));
 
 export { client };
