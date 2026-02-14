e683ed7eda8d38593dc38379ab730a5127836ab6
André Schlichting <andresch.dev@gmail.com>
2026-02-14 15:27:33 -0300

    Remove "supabase" client: use "pg" instead

--- server/src/infra/pg/connection.ts
+++ server/src/infra/pg/connection.ts
@@ -1,22 +1,6 @@
 import "dotenv/config";
 
-import z from "zod";
 import { Client } from "pg";
-import { createClient } from "@supabase/supabase-js";
-
-const envSchema = z.object({
-  DATABASE_URL: z.string(),
-  DATABASE_KEY: z.string(),
-});
-
-try {
-  if (process.env.NODE_ENV == "production") {
-    const env = envSchema.parse(process.env);
-    createClient(env.DATABASE_URL, env.DATABASE_KEY);
-  }
-} catch (error) {
-  console.error('Database authentication failed.\n', error);
-}
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
